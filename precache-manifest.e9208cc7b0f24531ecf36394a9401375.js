86d39cf4a39fc168ae2927119c855b0ea8e7fd7e
Daniel Mason <danmasson0@gmail.com>
2022-07-18 10:52:16 -0400

    Updates

---
+++ precache-manifest.e9208cc7b0f24531ecf36394a9401375.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6de3aa749ab1d0118e29d2b34af1ed76",
+    "url": "/slider-react-hook-practise/index.html"
+  },
+  {
+    "revision": "4053e4a415c7e1906bd1",
+    "url": "/slider-react-hook-practise/static/css/main.b350ebb3.chunk.css"
+  },
+  {
+    "revision": "61c9a72a86221f692a54",
+    "url": "/slider-react-hook-practise/static/js/2.611ee867.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/slider-react-hook-practise/static/js/2.611ee867.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4053e4a415c7e1906bd1",
+    "url": "/slider-react-hook-practise/static/js/main.eff19a30.chunk.js"
+  },
+  {
+    "revision": "5e46b14cb517a92555ba",
+    "url": "/slider-react-hook-practise/static/js/runtime-main.e76f4552.js"
+  }
+]);
